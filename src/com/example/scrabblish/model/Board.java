7003def048302802e49246f87ff1e29310edf480
Dylan Barth <dylan.j.barth@gmail.com>
2013-05-12 11:20:23 -0700

    Dynamically calculating the score now

--- src/com/example/scrabblish/model/Board.java
+++ src/com/example/scrabblish/model/Board.java
@@ -2,6 +2,7 @@ package com.example.scrabblish.model;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.TreeMap;
 
 import android.annotation.SuppressLint;
@@ -268,135 +269,50 @@ public class Board {
 		Log.d(TAG, words.toString());
 		for(int i=0; i < words.length; i++){
 			Log.d(TAG, words[i]);
-			//			score += calcWordScore(words[i]);
+			score += calcWordScore(words[i]);
 		}
 		return score;
 	}
-
-	//	public int calculateScore() {
-	//		showOccupation();
-	//		ArrayList<ArrayList<int[]>> allWordCoords = new ArrayList<ArrayList<int[]>>();
-	//		// Start in top left
-	//		int[] position = {0, 0};
-	//		// this loop checks for all horizontal words
-	////		Log.d(TAG, "Starting calcScore horizontal loop");
-	//		for(int i=0; i<this.gridLength; i++){
-	//			position[0] = i; // rows
-	////			Log.d(TAG, "Position: " + position[0] + ", " + position[1]);
-	//			for(int j=0; j<this.gridLength; j++){
-	////				Log.d(TAG, "i:" + i + ", j:" + j);
-	//				Log.d(TAG, "Position about to be checked: " + position[0] + ", " + position[1]);
-	//				ArrayList<int[]> wordCoords = checkForWord("horizontal", position[0], position[1]);
-	////				Log.d(TAG, "Got wordCoords from checkForWord");
-	//				if(wordCoords != null){
-	////					Log.d(TAG, "wordCoords != null");
-	//					allWordCoords.add(wordCoords);
-	////					Log.d(TAG, "Added wordCoords to master arrayList");
-	//					int[] lastElement = wordCoords.get(wordCoords.size() - 1);
-	////					Log.d(TAG, "lastElement: " + lastElement[1] + ", " + lastElement[0]);
-	//					j = lastElement[0]+1;
-	//					position[1] = j;
-	//				} else {
-	////					Log.d(TAG, "wordCoords = null");
-	//					position[1] = j;
-	//				}
-	//			}
-	//		}
-	//		//TODO
-	//		// add a loop that checks for vertical words
-	////		Log.d(TAG, "Starting calcScore vertical loop");
-	////		for(int i=0; i<this.size; i++){
-	////			position[1] = i; // cols
-	////			Log.d(TAG, "Position: " + position[0] + ", " + position[1]);
-	////			for(int j=0; j<this.size; j++){
-	////				Log.d(TAG, "i: " + i + ", j: " + j);
-	////				ArrayList<int[]> wordCoords = checkForWord("vertical", position[1], position[0]);
-	////				Log.d(TAG, "Got wordCoords from checkForWord");
-	////				if(wordCoords != null){
-	////					Log.d(TAG, "wordCoords = null");
-	////					allWordCoords.add(wordCoords);
-	////					Log.d(TAG, "Added wordCoords to master arrayList");
-	////					j = wordCoords.get(wordCoords.size() - 1)[1];
-	////					position[0] = j;
-	////				} else {
-	////					Log.d(TAG, "wordCoords != null");
-	////					position[0] = j + 1;
-	////				}
-	////				Log.d(TAG, "Incremented j: " + j);
-	////			}
-	////		}
-	//		// if valid words, add their score to total
-	//		// if not valid, make all of those tilespaces a weird color and don't summate that score.
-	//		int score = 0;
-	//		Log.d(TAG, "All word coords: ");
-	//		for(int i=0; i < allWordCoords.size(); i++){
-	//			ArrayList<int[]> wordCoords = allWordCoords.get(i);
-	//			String coords = "";
-	//			for(int j=0; j < wordCoords.size(); j++){
-	//				coords += "(" + wordCoords.get(j)[0] + ", " + wordCoords.get(j)[1] + "), ";
-	//			}
-	//			Log.d(TAG, coords);
-	//		}
-	//		return score;
-	//	}
-	//
-	//	public ArrayList<int[]> checkForWord(String direction, int row, int col){
-	//		// return an array list of coords if a word exists, otherwise return null
-	////		Log.d(TAG, "CheckForWord - direction: " + direction + ", coords:" + row + ", " + col);
-	//		TileSpace tileSpace;
-	//		ArrayList<int[]> wordCoords = null;
-	//		try {
-	//			tileSpace = getTileSpace(row, col);
-	//		} catch (Exception e){
-	//			return null;
-	//		}
-	//		if(tileSpace.isOccupied()){
-	//			Log.d(TAG, "TileSpace is occupied");
-	//			wordCoords = new ArrayList<int[]>();
-	//			while(tileSpace.isOccupied()){
-	//				int[] position = {row, col};
-	//				wordCoords.add(position);
-	//				Log.d(TAG, "Coords (" + row + ", " + col + ") added to wordCoords.");
-	//				if(direction == "horizontal"){
-	//					row++;
-	//				} else if (direction == "vertical"){
-	//					col++;
-	//				}
-	////				Log.d(TAG, "Checking next space at " + row + ", " + col);
-	//				try {
-	//					tileSpace = getTileSpace(row, col);
-	//				} catch (Exception e){
-	//					break; // indexed outside of tileSpace
-	//				}
-	//			}
-	//		} 
-	////		Log.d(TAG, "Got outside of the whileLoop");
-	//		if(wordCoords != null && wordCoords.size() > 1){
-	////			Log.d(TAG, "Returning words coords");
-	////			try {
-	////				printWord(wordCoords);
-	////			} catch (Exception e){
-	////				// do nothing
-	////			}
-	//			String coords = "";
-	//			for(int i=0; i<wordCoords.size(); i++){
-	//				coords += "(" + wordCoords.get(i)[0] + ", " + wordCoords.get(i)[1] + ") ";
-	//			}
-	//			Log.d(TAG, "Coords to be returned: " + coords);
-	//			return wordCoords;
-	//		} else {
-	////			Log.d(TAG, "Returning null");
-	//			return null;
-	//		}
-	//
-	//	}
-	//
-	//	public void printWord(ArrayList<int[]> wordCoords){
-	//		String word = "";
-	//		for(int[] i : wordCoords){
-	//			TileSpace tileSpace = getTileSpace(i[0], i[1]);
-	//			word += tileSpace.getCurrentTile().getLetter();
-	//		}
-	//		Log.d(TAG, "Word created: " + word);
-	//	}
+	
+	public int calcWordScore(String word){
+		int score = 0;
+		for(int i=0; i < word.length(); i++){
+			score += returnLetterValue(word.charAt(i));
+			Log.d(TAG, "***SCORE*** == " + score);
+		}
+		Log.d(TAG, "***SCORE*** == " + score);
+		return score;
+	}
+	
+	public int returnLetterValue(char c){
+		HashMap<Character, Integer> tileValues = new HashMap<Character, Integer>();
+		tileValues.put('A', 1);
+		tileValues.put('B', 3);
+		tileValues.put('C', 3);
+		tileValues.put('D', 2);
+		tileValues.put('E', 1);
+		tileValues.put('F', 4);
+		tileValues.put('G', 2);
+		tileValues.put('H', 4);
+		tileValues.put('I', 1);
+		tileValues.put('J', 8);
+		tileValues.put('K', 5);
+		tileValues.put('L', 1);
+		tileValues.put('M', 3);
+		tileValues.put('N', 1);
+		tileValues.put('O', 1);
+		tileValues.put('P', 3);
+		tileValues.put('Q', 10);
+		tileValues.put('R', 1);
+		tileValues.put('S', 1);
+		tileValues.put('T', 1);
+		tileValues.put('U', 1);
+		tileValues.put('V', 1);
+		tileValues.put('W', 4);
+		tileValues.put('X', 8);
+		tileValues.put('Y', 4);
+		tileValues.put('Z', 10);
+		int value = (Integer) tileValues.get(c);
+		return value;
+	}
 }
